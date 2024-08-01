38887324b019865929e7d18631648b319c09d65a
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-08-01 10:11:34 +0530

    Table added

--- Second/models.py
+++ Second/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=80)
+    Rollno = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.fname}"
+    
+class meta:
+    db_name = "Student"
