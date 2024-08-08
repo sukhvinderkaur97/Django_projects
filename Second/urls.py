2dd09ccfef68151a3d9ffe98aa471b43f72145d9
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-08-08 09:33:53 +0530

    linking webpage

--- Second/urls.py
+++ Second/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import xyz, abc
+from .views import xyz, abc, pqr, lmn
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first/', xyz),
+    path('second/', abc),
+    path('third/', pqr,  name = "temp1"),
+    path('fourth/', lmn,  name = "temp2"),
 ]
--- Second/views.py
+++ Second/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Student
 
 def xyz(request):
     return render(request, 'a.html')
 
 def abc(request):
-    return render(request, 'b.html')
+    P = Student.objects.all().values()
+    return render(request, 'b.html', {'std':P})
+
+def pqr(request):
+    return render(request, 'c.html')
+
+def lmn(request):
+    return render(request, 'd.html')
